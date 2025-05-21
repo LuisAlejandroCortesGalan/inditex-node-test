9414b8d968b8af8ca1fcc3afa3d596e623646553
LuisAlejandroCortesGalan <wincho_04@hotmail.com>
2025-05-21 14:31:38 +0200

    fix: stop tracking node_modules and dist

--- dist/middleware/requestLogger.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-declare module 'http' {
-  interface OutgoingMessage {
-    __responseStartTime?: number;
-  }
-}
-export declare const requestIdMiddleware: (req: Request, res: Response, next: NextFunction) => void;
-export declare const requestLogger: (req: Request, res: Response, next: NextFunction) => void;
